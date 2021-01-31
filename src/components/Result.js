44990401ab23e43e3d1b007b3a849709d45bdc05
ozawamaria <2ty2maria1999@gmail.com>
2021-02-01 00:21:48 +0900

    UI

---
+++ src/components/Result.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Result = ({result}) =>{
+    return(
+        <div className='resultValue'>
+            {result}
+        </div>
+    );
+};
+
+export default Result;
